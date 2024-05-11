d714497cebc76072f535e21134e8971f6fb577ec
DiegoHick <Khangchau2262003@gmail.com>
2024-05-11 23:13:11 +0700

    update

--- src/main/java/oop/project/qldiemthi/StatisticController.java
+++ src/main/java/oop/project/qldiemthi/StatisticController.java
@@ -1,4 +1,25 @@
 package oop.project.qldiemthi;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class StatisticController {
+
+
+
+    public void returnHome(MouseEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+            Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
